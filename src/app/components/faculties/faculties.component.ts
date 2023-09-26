383d1201544a38a6d1e230be13fd209c365b503b
Hari sankar <harisankarhari8921@gmail.com>
2023-09-26 19:54:56 +0530

    faculties

--- src/app/components/faculties/faculties.component.ts
+++ src/app/components/faculties/faculties.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-faculties',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./faculties.component.css']
 })
 export class FacultiesComponent {
+  faculties:any=[]
+constructor(private api:ApiService){}
 
+ngOnInit(){
+  this.api.getAllFaculties().subscribe((res)=>{
+   this.faculties=res.faculties
+  })
+}
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,11 +4,13 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class VideosService {
+export class ApiService {
 
   constructor(private http:HttpClient) { }
-  videosApi(){
-   return this.http.get<any>('assets/Api/videos.json')
+
+
+
+  getAllFaculties(){
+    return this.http.get<any>("./assets/Api/faculties.json")
   }
-  
 }
